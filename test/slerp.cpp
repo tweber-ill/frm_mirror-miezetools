db132f38f793f2c72aee52220193bfd3246c39fa
Tobias Weber <tobias.weber@frm2.tum.de>
2014-02-28 13:35:13 +0100

    added sample rotation stuff

--- test/slerp.cpp
+++ test/slerp.cpp
@@ -1,43 +1,31 @@
 #include <cmath>
 #include <boost/math/quaternion.hpp>
+#include "../helper/math.h"
+#include "../helper/linalg.h"
 
 using namespace boost::math;
 
-
-template<typename T>
-T quat_angle(const quaternion<T>& q1, const quaternion<T>& q2)
-{
-	T dot = q1.R_component_1()*q2.R_component_1() +
-		q1.R_component_2()*q2.R_component_2() +
-		q1.R_component_3()*q2.R_component_3() +
-		q1.R_component_4()*q2.R_component_4();
-
-	dot /= norm(q1);
-	dot /= norm(q2);
-
-	return std::acos(dot);
-}
-
-// see: http://run.usc.edu/cs520-s12/assign2/p245-shoemake.pdf
-template<typename T>
-quaternion<T> slerp(const quaternion<T>& q1, const quaternion<T>& q2, T t)
-{
-	T angle = quat_angle(q1, q2);
-
-	quaternion<T> q = std::sin((1-t)*angle)/std::sin(angle) * q1 +
-			std::sin(t*angle)/std::sin(angle) * q2;
-
-	return q;
-}
-
-
 int main()
 {
+	/*
 	quaternion<double> q1(1.,1.,1.,1.);
 	quaternion<double> q2(1.,2.,2.,2.);
 
 	quaternion<double> q = slerp(q1, q2, 1.);
 	std::cout << q << std::endl;
+	*/
+
+	ublas::vector<double> v1(3);
+	v1[0] = 0.; v1[1] = 1.; v1[2] = 0.;
+	ublas::vector<double> v2(3);
+	v2[0] = 0.; v2[1] = 0.; v2[2] = 1.;
+
+	for(double d=0.; d<=1.; d+=0.1)
+	{
+		ublas::vector<double> v0 = lerp(v1, v2, d);
+		ublas::vector<double> v = slerp(v1, v2, d);
+		std::cout << "lerp: " << v0 << ", slerp: " << v << std::endl;
+	}
 
 	return 0;
 }
