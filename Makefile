cc1dcefba26cd93d3cb19a2052da32adc11e99cc
Tobias Weber <tobias.weber@frm2.tum.de>
2015-01-09 10:42:58 +0100

    updated setup script

--- Makefile
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo "----------Building project:[ cattus - Debug ]----------"
-	@$(MAKE) -f  "cattus.mk"
-clean:
-	@echo "----------Cleaning project:[ cattus - Debug ]----------"
-	@$(MAKE) -f  "cattus.mk" clean
--- setup_tlibs.sh
+++ setup_tlibs.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 TLIBS=tlibs-master.tar.bz2
+TLIBS_DIR=tlibs
+
+if [ -L ${TLIBS_DIR} ]
+then
+	echo -e "Error: Symbolic link \"${TLIBS_DIR}\" exists. Aborting.";
+	exit -1;
+fi
 
 rm ${TLIBS}
 
@@ -16,5 +23,5 @@ then
 	exit -1;
 fi
 
-mv tlibs-master tlibs
+mv tlibs-master ${TLIBS_DIR}
 rm ${TLIBS}
